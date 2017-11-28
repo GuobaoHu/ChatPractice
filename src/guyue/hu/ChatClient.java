0cb08945d1a496c155d1eeaac85e8ab72af29d30
guyuehu <398456458@qq.com>
2017-11-28 21:37:41 +0800

    初始化项目

---
+++ src/guyue/hu/ChatClient.java
@@ -0,0 +1,12 @@
+package guyue.hu;
+
+import java.awt.*;
+
+/**
+ * @author guyue
+ * @date 2017年11月28日 下午9:16:10
+ * @class describ:
+ */
+public class ChatClient extends Frame {
+	
+}
---
+++ src/guyue/hu/ChatServer.java
@@ -0,0 +1,9 @@
+package guyue.hu;
+/**
+ * @author guyue
+ * @date 2017年11月28日 下午9:37:04
+ * @class describ:
+ */
+public class ChatServer {
+
+}
