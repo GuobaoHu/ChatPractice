46b5daa620fa0e4598bfa22ec88ec0f1f559cdf6
hgb22613 <hgb22613@SJ-010014.wsdc.com>
2017-11-29 15:40:28 +0800

    完成Chat的设计,主要技术点：1.Server端接收一个客户端，就起一个线程；2.在Server的线程中，接收客户端发送的数据，并将接收到的数据转发给每一个连接上来的客户端；3.在Client端需要向Server端发送数据；4.Client端起一个线程无限接收Server端发送的数据

--- src/guyue/hu/ChatClient.java
+++ src/guyue/hu/ChatClient.java
@@ -1,6 +1,9 @@
 package guyue.hu;
 
 import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
+import java.net.*;
 
 /**
  * @author guyue
@@ -8,5 +11,96 @@ import java.awt.*;
  * @class describ:
  */
 public class ChatClient extends Frame {
+	private TextField inputField = new TextField();
+	private TextArea showArea = new TextArea();
+	private Socket socket = null;
+	private DataOutputStream dos = null;
+	private DataInputStream dis = null;
+	private boolean isReceive = false;
+	private Thread t = new Thread(new Receive());
+
+	public static void main(String[] args) {
+		new ChatClient().launch();
+	}
+
+	// 启动
+	public void launch() {
+		this.setBounds(100, 100, 300, 300);
+		this.add(inputField, BorderLayout.SOUTH);
+		this.add(showArea, BorderLayout.NORTH);
+		showArea.setEditable(false);
+		this.pack();
+		this.addWindowListener(new Closing());
+		inputField.addActionListener(new EnterListener());
+		this.connect();
+		t.start();
+		this.setVisible(true);
+	}
 	
+	public void connect() {
+		try {
+			socket = new Socket("127.0.0.1", 18888);
+			dos = new DataOutputStream(socket.getOutputStream());
+			dis = new DataInputStream(socket.getInputStream());
+			isReceive = true;
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//添加关闭事件
+	private class Closing extends WindowAdapter {
+
+		@Override
+		public void windowClosing(WindowEvent e) {
+			try {
+				isReceive = false;
+				if(!(t.isAlive())) {
+					dos.close();
+					dis.close();
+					socket.close();
+				}
+				System.exit(0);
+			} catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		
+	}
+	
+	//响应TextField的Enter事件
+	private class EnterListener implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent arg0) {
+			try {
+				String str = inputField.getText();
+				dos.writeUTF(str);
+				dos.flush();
+				inputField.setText("");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
+	
+	//单独起一个线程无限接收Server发送过来的数据
+	private class Receive implements Runnable {
+
+		@Override
+		public void run() {
+			try {
+			while(isReceive) {
+				String str = dis.readUTF() + "\r\n";
+				showArea.setText(showArea.getText() + str);
+			}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+	}
 }
--- src/guyue/hu/ChatServer.java
+++ src/guyue/hu/ChatServer.java
@@ -1,9 +1,110 @@
 package guyue.hu;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
 /**
  * @author guyue
  * @date 2017年11月28日 下午9:37:04
  * @class describ:
  */
 public class ChatServer {
-
+	private ServerSocket serverSocket = null;
+	private boolean isConnect = false; //客户端是否连接
+	private List<Client> clients = new ArrayList<Client>();//收集各个连接上来的客户端，以向各客户端发送数据
+	
+	public static void main(String[] args) {
+		new ChatServer().launch();
+	}
+	
+	//启动Server端时启动ServerSocket
+	public void launch() {
+		try {
+			serverSocket = new ServerSocket(18888);
+			isConnect = true;
+			//从启动开始一直接收客户端
+			while(isConnect) {
+				Socket socket = serverSocket.accept();
+				Client c = new Client(socket);
+				clients.add(c);
+				new Thread(c).start();
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//每连接一个客户端，起一个线程，以下是线程类
+	private class Client implements Runnable {
+		private Socket socket = null;
+		private DataInputStream dis = null;
+		private DataOutputStream dos = null;
+		private boolean isReceive = false;
+		
+		public Client(Socket socket) {
+			this.socket = socket;
+			this.initialize();
+		}
+		
+		//初始化
+		public void initialize() {
+			try {
+				isReceive = true;
+				dis = new DataInputStream(socket.getInputStream());
+				dos = new DataOutputStream(socket.getOutputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		//发送数据
+		public void send(String str) {
+			try {
+				dos.writeUTF(str);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		
+		@Override
+		public void run() {
+			try {
+				while(isReceive) {
+					String str = dis.readUTF();
+System.out.println(str);
+					for(int i=0; i<clients.size(); i++) {
+						Client c = clients.get(i);
+						c.send(str);
+					}
+				}
+			} catch (EOFException e) {
+				System.out.println("A client quit!");
+			}
+			catch (SocketException e) {
+				clients.remove(this);
+				System.out.println("A client quit!");
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					isReceive = false;
+					if(dis != null) {
+						dis.close();
+					}
+					if(dos != null) {
+						dos.close();
+					}
+					if(socket != null) {
+						socket.close();
+					}
+				} catch (IOException ex) {
+					ex.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
 }
